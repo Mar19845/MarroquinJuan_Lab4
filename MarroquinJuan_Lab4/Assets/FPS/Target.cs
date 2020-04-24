458a423351b7b0f80f4249d2e3fefaca1c321b4a
Mar19845 <54874288+Mar19845@users.noreply.github.com>
2020-04-24 04:29:43 -0600

    Disparo, destruccion de objetos y mas

---
+++ MarroquinJuan_Lab4/Assets/FPS/Target.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health  = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if(health<= 0)
+        {
+            Morir();
+        }
+    }
+    void Morir()
+    {
+        Destroy(gameObject);
+    }
+}
